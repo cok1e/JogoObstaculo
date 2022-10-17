bf3379e7afdfc04cccc6ec69a7fee8f52018e8f0
Chris <111128830+cok1e@users.noreply.github.com>
2022-10-17 08:25:31 -0300

    oi

--- js/jogo.js
+++ js/jogo.js
@@ -1,19 +1,19 @@
 let  canvas, ctx, ALTURA, LARGURA, frames = 0, maxPulos = 3, velocidade = 12, estadoAtual,
 
    estados = {
-    jogar: 0,
+    jogar: 0,   //mostra quantas vezes perdeu,se ganhou ou se está jogando
     jogando: 1,
     perdeu:2,
   },
 
    chao = {
     y: 550,
-    altura:50,
+    altura:50,  //código do chão
     cor: "#e8da78",
 
     desenha(){
       ctx.fillStyle = this.cor
-      ctx.fillRect( 0, this.y, LARGURA, this.altura )
+      ctx.fillRect( 0, this.y, LARGURA, this.altura ) //design do jogo
     },
   },
 
@@ -22,7 +22,7 @@ let  canvas, ctx, ALTURA, LARGURA, frames = 0, maxPulos = 3, velocidade = 12, es
     y: 0,
     altura:50,
     largura:50,
-    cor: "#ff9239",
+    cor: "#ff9239",   //código do personagem jogado
     gravidade: 1.6,
     velocidade: 0,
     forcaDoPulo:23.6 ,
@@ -33,15 +33,15 @@ let  canvas, ctx, ALTURA, LARGURA, frames = 0, maxPulos = 3, velocidade = 12, es
       this.velocidade += this.gravidade
       this.y += this.velocidade
 
-      if( this.y > chao.y - this.altura && estadoAtual != estados.perdeu  ){
-        this.y = chao.y - this.altura 
+      if( this.y > chao.y - this.altura && estadoAtual != estados.perdeu  ){    //código de atualização quando perde ou atualiza
+                this.y = chao.y - this.altura 
         this.qntPulos =  0    
         this.velocidade = 0
       }
     },
 
     pula(){
-      if(this.qntPulos < maxPulos){
+      if(this.qntPulos < maxPulos){   //código de função pular
         this.velocidade = -this.forcaDoPulo
         this.qntPulos++
       }
@@ -49,33 +49,33 @@ let  canvas, ctx, ALTURA, LARGURA, frames = 0, maxPulos = 3, velocidade = 12, es
 
     reset(){
       this.velocidade = 0
-      this.y  = 0
+      this.y  = 0   //código para função resetar
       this.scorre = 0
     },
 
     desenha(){
       ctx.fillStyle = this.cor
-      ctx.fillRect( this.x, this.y, this.altura, this.largura )
+      ctx.fillRect( this.x, this.y, this.altura, this.largura )  //código de desenho
     },
   },
 
  obstaculos = {
   _obs: [],
-  cores: ["#ffbc1c", "#ff1c1c", "#ff85e1", "#52a7ff", "#78ff4d"],
+  cores: ["#ffbc1c", "#ff1c1c", "#ff85e1", "#52a7ff", "#78ff4d"], //código da função dos obstaculos
   tempoInsere: 0,
 
   insere() {
     this._obs.push({
       x: LARGURA,
       largura: 50,
-      altura: 30 + Math.floor(120 * Math.random()),
-      cor: this.cores[Math.floor(5 * Math.random())],
+      altura: 30 + Math.floor(120 * Math.random()), 
+      cor: this.cores[Math.floor(5 * Math.random())], 
     })
     this.tempoInsere = 30 + Math.floor(22 * Math.random())
   },
 
   atualiza() {
-    if(this.tempoInsere ==  0)
+    if(this.tempoInsere ==  0)   //código de atualização
       this.insere()
     else
       this.tempoInsere--
@@ -108,7 +108,7 @@ let  canvas, ctx, ALTURA, LARGURA, frames = 0, maxPulos = 3, velocidade = 12, es
     for(let i = 0, tam = this._obs.length; i < tam; i++) {
       let obs = this._obs[i]
       ctx.fillStyle = obs.cor
-      ctx.fillRect(obs.x, chao.y - obs.altura, obs.largura, obs.altura)
+      ctx.fillRect(obs.x, chao.y - obs.altura, obs.largura, obs.altura) //desenho
     }
   },
 }
@@ -120,7 +120,7 @@ function clique(){
   else if(estadoAtual == estados.jogar)
     estadoAtual = estados.jogando
 
-  else if(estadoAtual == estados.perdeu && bloco.y >= 2 * ALTURA){
+  else if(estadoAtual == estados.perdeu && bloco.y >= 2 * ALTURA){ //função de clicar
     estadoAtual = estados.jogar
     obstaculos.limpa()
     bloco.reset()
@@ -132,19 +132,19 @@ function roda(){
   atualiza()
   desenha()
 
-  window.requestAnimationFrame(roda)
+  window.requestAnimationFrame(roda) //código para rodar a animação
 }
 
 function atualiza(){
   frames++
   bloco.atualiza()
-  if ( estadoAtual == estados.jogando)
+  if ( estadoAtual == estados.jogando) //código para função de atualizar
     obstaculos.atualiza()
 }
 
 function desenha() {
   ctx.fillStyle = "#80daff"
-  ctx.fillRect(0, 0, LARGURA, ALTURA)
+  ctx.fillRect(0, 0, LARGURA, ALTURA) //código para funcionar o desenho
 
   ctx.fillStyle = "#fff"
   ctx.font = "50px Arial"
@@ -155,11 +155,11 @@ function desenha() {
 
     case estados.jogar:
       ctx.fillStyle = "green"
-      ctx.fillRect(LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100)
+      ctx.fillRect(LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100)// código do botão quando começa
       break
     case estados.perdeu:
       ctx.fillStyle = "red"
-      ctx.fillRect(LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100)
+      ctx.fillRect(LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100)  //mostra o botão quando perde
 
       ctx.save()
       ctx.translate(LARGURA / 2, ALTURA / 2)
@@ -192,7 +192,7 @@ function main() {
         ALTURA = 600;
     }
 
-    canvas = document.createElement("canvas")
+    canvas = document.createElement("canvas")  //é o local de início (entry point) da execução de um programa
     canvas.width = LARGURA
     canvas.height = ALTURA
     canvas.style.border = "1px solid #000"
